cb3fd5bfa3ee7e9b18278e570b49b925d6f12677
Andreia Domingues <andreianetface@gmail.com>
2024-05-31 18:10:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['Javascript', 'Node.js', 'React.js', 'MongoDB']
+    };
+    return member;
+}   
